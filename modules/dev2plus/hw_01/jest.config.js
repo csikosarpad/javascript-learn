12cf3318c0b86b5ade4c561665fd0e6628c99f63
Csikós Árpád <arpad.csikos@gmail.com>
2021-05-03 07:53:19 +0200

    jest config, coverage

--- modules/dev2plus/hw_01/jest.config.js
+++ modules/dev2plus/hw_01/jest.config.js
@@ -1,3 +1,12 @@
-const config = {
+module.exports = {
   verbose: true,
+  coverageReporters: ["text", "json", "html", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
