b12f200a59d56fd9bf2e2a91be280a1a21cbe738
Csikós Árpád <arpad.csikos@gmail.com>
2021-06-22 23:05:05 +0200

    init, simple react based repo

---
+++ modules/dev2plus/hw_06/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  target: "node",
+  devServer: {
+    port: "3000",
+    contentBase: ["./public"],
+    open: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
